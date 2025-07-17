f0d71196b3388fd91759f9aabb91d67742c77374
Jash Doshi <jashdoshi279@gmail.com>
2025-07-17 15:45:07 +0530

    done

--- dashboard.py
+++ dashboard.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.title("edited")
-
 st.set_page_config(page_title="College Info Dashboard", layout="wide")
 st.title("College Information Dashboard")
 alt.themes.enable("dark")
